371e9051607a6be1861adc0e03fad08781ae0192
Cedric-song <kengmun@126.com>
2017-01-12 16:23:22 +0800

    create a project of wechatCX named Cotton Town

---
+++ page/index/index.js
@@ -0,0 +1,7 @@
+//index.js
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {}
+  }
+})
---
+++ page/plaza/index.js
@@ -0,0 +1,6 @@
+//index.js
+Page({
+onLaunch: function () {
+    console.log('App Launch')
+  }
+})

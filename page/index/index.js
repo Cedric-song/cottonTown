7dbea2d4bdc51ba0fa3f04f771519060729e992a
Cedric-song <kengmun@126.com>
2017-01-21 17:03:57 +0800

    add some page

--- page/index/index.js
+++ page/index/index.js
@@ -1,7 +1,117 @@
-//index.js
+const swiperData = [
+      {
+        swiperId:"1",
+        swiperName:"活动一",
+        swiperImageUrl:"http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+        gotoMethod:"navgator",
+        gotoUrl:"/page/active/active?id=1"
+      },
+      {
+        swiperId:"2",
+        swiperName:"活动二",
+        swiperImageUrl:"http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg",
+        gotoMethod:"navgator",
+        gotoUrl:"/page/active/active?id=1"
+      },
+      {
+        swiperId:"3",
+        swiperName:"活动三",
+        swiperImageUrl:"http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg",
+        gotoMethod:"navgator",
+        gotoUrl:"/page/active/active?id=1"
+      }    
+    ]
+
+
+const listData = [{
+      "topicId": "1",
+      "topicName": "10天早起计划",
+      "topicType": "打卡小组",
+      "topicDescription": "如果你还无法简洁的表达你的想法，那只说明你还不够了解它",
+      "topicImageUrl": "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+      "topicTimeStamp": 1484892814514
+    },
+    {
+      "topicId": "2",
+      "topicName": "10天阅读好习惯",
+      "topicType": "课程",
+      "topicDescription": "如果你还无法简洁的表达你的想法，那只说明你还不够了解它如果你还无法简洁的表达你的想法，那只说明你还不够了解它",
+      "topicImageUrl": "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+      "topicTimeStamp": 1484892814516
+    },
+    {
+      "topicId": "2",
+      "topicName": "10天阅读好习惯",
+      "topicType": "课程",
+      "topicDescription": "如果你还无法简洁的表达你的想法，那只说明你还不够了解它如果你还无法简洁的表达你的想法，那只说明你还不够了解它",
+      "topicImageUrl": "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+      "topicTimeStamp": 1484892814516
+    },
+    {
+      "topicId": "2",
+      "topicName": "10天阅读好习惯",
+      "topicType": "课程",
+      "topicDescription": "如果你还无法简洁的表达你的想法，那只说明你还不够了解它如果你还无法简洁的表达你的想法，那只说明你还不够了解它",
+      "topicImageUrl": "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+      "topicTimeStamp": 1484892814516
+    },
+    {
+      "topicId": "2",
+      "topicName": "10天阅读好习惯",
+      "topicType": "课程",
+      "topicDescription": "如果你还无法简洁的表达你的想法，那只说明你还不够了解它如果你还无法简洁的表达你的想法，那只说明你还不够了解它",
+      "topicImageUrl": "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+      "topicTimeStamp": 1484892814516
+    },
+    {
+      "topicId": "2",
+      "topicName": "10天阅读好习惯",
+      "topicType": "课程",
+      "topicDescription": "如果你还无法简洁的表达你的想法，那只说明你还不够了解它如果你还无法简洁的表达你的想法，那只说明你还不够了解它",
+      "topicImageUrl": "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+      "topicTimeStamp": 1484892814516
+    },
+    {
+      "topicId": "2",
+      "topicName": "10天阅读好习惯",
+      "topicType": "课程",
+      "topicDescription": "如果你还无法简洁的表达你的想法，那只说明你还不够了解它如果你还无法简洁的表达你的想法，那只说明你还不够了解它",
+      "topicImageUrl": "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+      "topicTimeStamp": 1484892814516
+    },
+    {
+      "topicId": "2",
+      "topicName": "10天阅读好习惯",
+      "topicType": "课程",
+      "topicDescription": "如果你还无法简洁的表达你的想法，那只说明你还不够了解它如果你还无法简洁的表达你的想法，那只说明你还不够了解它",
+      "topicImageUrl": "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+      "topicTimeStamp": 1484892814516
+    },
+    {
+      "topicId": "2",
+      "topicName": "10天阅读好习惯",
+      "topicType": "课程",
+      "topicDescription": "如果你还无法简洁的表达你的想法，那只说明你还不够了解它如果你还无法简洁的表达你的想法，那只说明你还不够了解它",
+      "topicImageUrl": "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+      "topicTimeStamp": 1484892814516
+    }]
+
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    swiperData: swiperData,
+    swiperConfig:{
+      indicatorDots: true,
+      autoplay: true,
+      circular: true,
+      interval: 5000,
+      duration: 1000,
+      supportGoto: false
+    },
+    
+    listData: listData
+  },
+  goto (e) {
+    const data = e.target.dataset
+    console.log(data)
   }
 })
